5247aca8753498588a11940277ae63718dc55205
linknn <crysnnelson@hotmail.com>
2025-08-24 22:17:04 -0700

    Style Item Cards

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,5 +1,12 @@
+import "../blocks/ItemCard.css";
+
 function ItemCard({ data }) {
-  return <>{data.name}</>;
+  return (
+    <li className="card">
+      <h2 className="card__title">{data.name}</h2>
+      <img src={data.link} alt={data.name} className="card__image" />
+    </li>
+  );
 }
 
 export default ItemCard;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,17 @@
 import WeatherCard from "./WeatherCard";
 import ItemCard from "./ItemCard";
+import "../blocks/Main.css";
 
 function Main({ clothingItems }) {
   return (
     <main className="main">
       <WeatherCard />
       <p className="main__text">Today is 75° F / You may want to wear:</p>
-      {clothingItems.map((item) => {
-        return <ItemCard data={item} />;
-      })}
+      <ul className="main__card-list">
+        {clothingItems.map((item) => {
+          return <ItemCard key={item._id} data={item} />;
+        })}
+      </ul>
     </main>
   );
 }
