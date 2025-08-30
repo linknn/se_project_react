d30bd0a7a87fe4916daa5bf32671de61fde52fa8
linknn <crysnnelson@hotmail.com>
2025-08-29 21:53:06 -0700

    Style the form modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,15 +42,15 @@ function App() {
         <fieldset className="modal__fieldset">
           <label htmlFor="add-clothes-name" className="modal__label">
             Name
-            <input id="add-clothes-name" type="text" className="modal__input" />
+            <input id="add-clothes-name" type="text" className="modal__input" placeholder="Name" />
           </label>
           <label className="modal__label">
             Image
-            <input type="url" className="modal__input" />
+            <input type="url" className="modal__input" placeholder="Image URL" />
           </label>
         </fieldset>
         <fieldset className="modal__fieldset">
-          <legend>Select the weather type:</legend>
+          <legend className="modal__form-legend">Select the weather type:</legend>
 
           <div>
             <input className="modal__radio-btn" type="radio" id="hot" name="weather" value="hot" />
