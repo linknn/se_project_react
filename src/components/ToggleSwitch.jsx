1915f8b443eac77cd73777331e4d7815c5aaf93e
linknn <crysnnelson@hotmail.com>
2025-09-02 11:48:48 -0700

    :sparkle:Toggle switch circle can move!

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+
 function ToggleSwitch() {
+  const [tempUnit, setTempUnit] = useState("F");
+
+  function handleChange() {
+    if (tempUnit == "F") {
+      setTempUnit("C");
+    } else {
+      setTempUnit("F");
+    }
+  }
+
+  useEffect(() => {
+    console.log(tempUnit);
+  });
+
   return (
     <label htmlFor="toggle-switch" className="toggle-switch">
-      <input id="toggle-switch" type="checkbox" className="toggle-switch__checkbox" />
+      <input
+        id="toggle-switch"
+        type="checkbox"
+        className="toggle-switch__checkbox"
+        onChange={handleChange}
+      />
       <span className="toggle-switch__circle"></span>
       <span className="toggle-switch__value toggle-switch__value_left">F</span>
       <span className="toggle-switch__value toggle-switch__value_right">C</span>
