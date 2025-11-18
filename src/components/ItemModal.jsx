d62d2864ca605cc163a74557171dc3c20a3115d2
linknn <crysnnelson@hotmail.com>
2025-11-18 04:43:40 -0800

    Create context for current user for delete function

--- src/components/ItemModal.jsx
+++ src/components/ItemModal.jsx
@@ -1,13 +1,19 @@
 import closeIcon from "../images/light-close.png";
 import closeIconDark from "../images/dark_close.svg";
 import { handleOutsideModalClick } from "../utils/modalFunctions";
-import { useState } from "react";
+import { useState, useContext } from "react";
+
+import CurrentUserContext from "../contexts/CurrentUserContext";
 
 function ItemModal({ card, isOpen, onClose, handleDeleteItem }) {
   const [isLoading, setIsLoading] = useState(false);
+  const currentUser = useContext(CurrentUserContext);
+
+  const isOwn = card.owner === currentUser?._id;
+
   function handleDelete() {
     setIsLoading(true);
-    handleDeleteItem(card).finally(() => {
+    handleDeleteItem().finally(() => {
       setTimeout(() => {
         setIsLoading(false);
       }, 500);
@@ -27,15 +33,20 @@ function ItemModal({ card, isOpen, onClose, handleDeleteItem }) {
         <button type="button" className="modal__close-btn_dark" onClick={onClose}>
           <img src={closeIconDark} alt="x-icon" />
         </button>
+
         <img src={card.imageUrl} alt={card.name} className="modal__image" />
+
         <div className="modal__footer">
           <div className="modal__text">
             <h2 className="modal__text-desc">{card.name}</h2>
             <p className="modal__text-desc">Weather: {card.weather}</p>
           </div>
-          <button onClick={handleDelete} className="modal__delete-btn">
-            {isLoading ? "Deleting..." : "Delete Item"}
-          </button>
+
+          {isOwn && (
+            <button onClick={handleDelete} className="modal__delete-btn">
+              {isLoading ? "Deleting..." : "Delete Item"}
+            </button>
+          )}
         </div>
       </div>
     </div>
